ac089447e005e8043d3337190b089b9a106f1896
daffahfdzz <daffahfdzz@gmail.com>
2026-08-03 23:28:39 +0700

    update portofolio website

--- script.js
+++ script.js
@@ -1,11 +1,90 @@
+// Tombol Scroll Down
 const tombol = document.getElementById("scrollBtn");
 
 tombol.addEventListener("click", () => {
-
     document.getElementById("about").scrollIntoView({
+        behavior: "smooth"
+    });
+});
+
+// Animasi Skill Bar
+window.addEventListener("load", () => {
+
+    document.querySelector(".html").style.width = "90%";
+
+    document.querySelector(".css").style.width = "80%";
+
+    document.querySelector(".js").style.width = "55%";
+
+});
+// Typing Animation
+
+const text = "High School Student • Athlete • Lifelong Learner";
+
+const typing = document.getElementById("typing");
+
+let index = 0;
+
+function ketik(){
+
+    if(index < text.length){
+
+        typing.innerHTML += text.charAt(index);
+
+        index++;
+
+        setTimeout(ketik,60);
+
+    }
+
+}
+
+ketik();
+// Back To Top
+
+const topBtn = document.getElementById("topBtn");
+
+window.addEventListener("scroll",()=>{
+
+    if(window.scrollY>300){
+
+        topBtn.style.display="block";
+
+    }else{
+
+        topBtn.style.display="none";
+
+    }
+
+});
+
+topBtn.addEventListener("click",()=>{
+
+    window.scrollTo({
+
+        top:0,
 
         behavior:"smooth"
 
     });
 
 });
+// Reveal Animation
+
+const hiddenElements = document.querySelectorAll(".hidden");
+
+const observer = new IntersectionObserver((entries)=>{
+
+entries.forEach(entry=>{
+
+if(entry.isIntersecting){
+
+entry.target.classList.add("show");
+
+}
+
+});
+
+});
+
+hiddenElements.forEach((el)=>observer.observe(el));
